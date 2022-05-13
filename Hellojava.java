e4aeb3693591162779e2879d507c4275a3094103
sunarayesha <96300505+sunarayesha@users.noreply.github.com>
2022-05-13 21:54:34 +0530

    Update Hellojava.java

--- Hellojava.java
+++ Hellojava.java
@@ -1,6 +1,6 @@
 class HelloJava
 {
 public static void main(String args[]) {
-System.out.println("My first java program for github--------jenkins git polling log");
+System.out.println("My first java program for github--------jenkins");
 }
 }
